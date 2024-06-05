e97cfcfb8fe8da92f6c674838a5cc86168ff67a3
Víctor Martínez <knoopx@gmail.com>
2024-06-05 11:23:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='mgds',
       description='Modular Pytorch DataSet implementation',
       author='Nerogar',
       url='https://github.com/Nerogar/stable-diffusion-dataloader/',
-      package_dir={'': 'src'},
+      package_dir={'': 'src/**/*'},
       packages=['mgds'],
       )
